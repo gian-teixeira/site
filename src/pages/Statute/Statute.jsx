f7acea46ebc291dc5b2e7f3c3d5a6d0dd5a86359
Gian <moreonmove@gmail.com>
2022-12-23 13:39:34 -0300

    estatuto

--- src/pages/Statute/Statute.jsx
+++ src/pages/Statute/Statute.jsx
@@ -4,15 +4,120 @@ function Statute(){
     return (
         <div id="statute">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac orci odio. Sed eleifend 
-                fermentum tortor a lobortis. Duis porta tempus odio at lobortis. Morbi mattis hendrerit 
-                lacus non interdum. Ut non libero sed mi pharetra pharetra et sit amet erat. Pellentesque 
-                habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc 
-                mattis ut erat sit amet bibendum. Sed efficitur quam diam, vitae convallis augue volutpat 
-                sit amet. Donec maximus dolor eu nisi vestibulum varius. In hac habitasse platea dictumst. 
-                Etiam a orci cursus, ultricies leo eget, mattis nisl. Vestibulum ac placerat nulla, a 
-                volutpat nibh. Aliquam a tempus elit.
+                Este estatuto tem como objetivo estabelecer normas para a equipe de maratona de programação do DCOMP/UFSJ.
             </p>
+
+            <h3>DISPOSIÇÕES PRELIMINARES</h3> 
+
+            <p>
+                A equipe de maratona de programação do DCOMP/UFSJ foi criada em outubro de 2022 e oficializada pelo departamento 
+                na 109a Reunião Ordinária do DCOMP/UFSJ. Os membros fundadores são:
+            </p>
+
+            <p><b>Coordenador: </b>Elverton Carvalho Fazzion</p>
+            <p><b>Discentes: </b>Giancarlo Oliveira Teixeira, Guilherme Bittencourt, Guilherme Fonseca do Nascimento, Yan Ribeiro Andrade</p>
+            <p><b>Orientadores: Elverton Carvalho Fazzion, Alexandre Bittencourt Pigozzo, Leonardo Chaves Dutra da Rocha</b></p>
+
+            <h3>OBJETIVOS</h3>
+
+            <p>
+                A equipe de maratona de programação do DCOMP/UFSJ tem como finalidade o treinamento para participação de edições da Maratona
+                Mineira de Programação e da Maratona Brasileira de Programação. 
+            </p>
+
+            <h3>DA EQUIPE</h3>
+
+            <ul>
+                <li>
+                    Das vagas
+                    <ul>
+                        <li>A equipe é composta por, no máximo, 12 discentes.</li>
+                        <li>Para assumir uma vaga em aberto na equipe, o candidato deverá 
+                            cumprir o treinamento básico estipulado no site oficial da equipe 
+                            e realizar uma entrevista com o Coordenador.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    Da elegibilidade
+                    <ul>
+                        <li>Poderá se candidatar para uma vaga na equipe alunos de qualquer curso da UFSJ.</li>
+                        <li>O aluno deverá ser elegível para participar de, pelo menos, uma edição da Maratona 
+                            Mineira de Programação e da  Maratona Brasileira de Programação.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    Das obrigações dos membros
+                    <ul>
+                        <li>Participar dos treinamentos da equipe.</li>
+                        <li>Comunicar problemas sobre o treinamento para o capitão da equipe.</li>
+                        <li>Comunicar problemas sobre infraestrutura para o coordenador da equipe.</li>
+                        <li>Colaborar com os outros membros da equipe.</li>
+                        <li>Respeitar e manter a cordialidade no ambiente.</li>
+                        <li>Manter bom desempenho acadêmico.</li>
+                        <li>Devolver qualquer material disponibilizado se desligado.</li>
+                        <li>Conhecer e cumprir as normas do regimento.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    Da seleção do capitão
+                    <ul>
+                        <li>O capitão será selecionado pelo coordenador da equipe levando em consideração o desempenho acadêmico, 
+                            desempenho em competições e tempo de participação na equipe.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    Das obrigações do capitão
+                    <ul>
+                        <li>Comunicar ao orientador qualquer problema ocorrido no ambiente do treinamento.</li>
+                        <li>Organizar os horários de treinamentos da equipe.</li>
+                        <li>Cumprir e fazer cumprir as normas deste estatuto.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <h3>DA DENOMINAÇÃO</h3>
+
+            <ul>
+                <li>
+                    Do nome da equipe
+                    <ul>
+                        <li>O nome oficial da equipe de maratona de programação do DCOMP/UFSJ  é "Sem Condições". </li>
+                        <li>O nome poderá ser alterado em comum acordo entre os membros da equipe.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    Do nome dos times
+                    <ul>
+                        <li>O nome dos times da equipe deverá iniciar com [XXXXXXX].</li>
+                        <li>O time composto pelos discentes fundadores poderá utilizar somente o nome da equipe.</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <h3>DO CERTIFICADO DE PARTICIPAÇÃO</h3>
+
+            <p>Os discentes receberão certificado de participação se integrarem a equipe de maratona por, pelo menos, 
+                seis meses e participarem de, ao menos, uma competição.</p>
+
+            <h3>DA MUDANÇA DO ESTATUTO</h3>
+
+            <p>Este estatuto poderá ser modificado com aprovação de dois terços de seus membros.</p>
+
         </div>
     );
 }
